0b5a0b687a7f0d57205a5e2a2e4b5d0d693b013b
junjun0817 <rakanpanera1201@gmail.com>
2023-12-24 12:51:17 +0900

    フロント環境構築　wip

---
+++ front-dir/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HeaderItem msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HeaderItem from './components/HeaderItem.vue'
+
+
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderItem
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front-dir/src/components/Letter.vue
@@ -0,0 +1,15 @@
+<template>
+
+<!--検索-->
+	<div class="form-memo">
+			<input placeholder="メモしたいことを入力">
+	</div>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
